2a719eab97b1c49b55ce580b75669706552b683f
Haeun Kim <gracekim.an@gmail.com>
2021-10-28 15:10:41 +0900

    adding unshift

--- LinkedList/SinglyLinkedList.js
+++ LinkedList/SinglyLinkedList.js
@@ -48,5 +48,17 @@ class SinglyLinkedList {
     this.length--;
     return currentHead;
   }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 var list = new SinglyLinkedList();
