6b5e6a6d0b156c1ace20e148101905aebb68dd27
Haeun Kim <gracekim.an@gmail.com>
2021-11-02 12:11:25 +0900

    Adding reverse

--- LinkedList/SinglyLinkedList.js
+++ LinkedList/SinglyLinkedList.js
@@ -98,5 +98,19 @@ class SinglyLinkedList {
     preNode.next = removes.next;
     return removes;
   }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 var list = new SinglyLinkedList();
